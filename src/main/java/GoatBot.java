472e64f192a78760b32aa62b949fa76634006bc2
Kaushal Rajesh <e1511862@u.nus.edu>
2026-08-26 14:45:12 +0800

    Implement bot echo feature

--- src/main/java/GoatBot.java
+++ src/main/java/GoatBot.java
@@ -1,9 +1,10 @@
 /**
  * Entry point for Goat Bot.
  */
+import java.util.Scanner;
 public class GoatBot {
     public static void main(String[] args) {
-        String banner = """
+        String welcomeBanner = """
                 ____________________________________________________________
                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
                 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⣿⣿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -23,9 +24,23 @@ public class GoatBot {
                 Hello! I'm Goat Bot.
                 What can I do for you?
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
+                """;
+        System.out.println(welcomeBanner);
+        Scanner scanner = new Scanner(System.in); //scanner object attached to stdin like keyboard
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(
+                    "\n____________________________________________________________\n" +
+                            userInput + "\n" +
+                    "____________________________________________________________");
+            userInput = scanner.nextLine();
+        }
+        // string if user says bye
+        String farewell = """
+                ____________________________________________________________
+                Bye. Hope to see you again soon! Happy hooping :)
                 ____________________________________________________________
                 """;
-        System.out.println(banner);
+        System.out.println(farewell);
     }
 }
