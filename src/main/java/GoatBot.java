c1017915f8b6f00f0308e832815242b3c4a09967
Kaushal Rajesh <e1511862@u.nus.edu>
2026-08-27 23:51:04 +0800

    Implemented Add, List functions

--- src/main/java/GoatBot.java
+++ src/main/java/GoatBot.java
@@ -21,26 +21,40 @@ public class GoatBot {
                 ⠀⠀⠀⢀⣠⣾⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣷⣆⣀⠀⢀⡀
                 ⢀⣤⣴⡿⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⠟⠋⠀
                 ⠀⠉⠀⠀⠀⠀⠀
-                Hello! I'm Goat Bot.
-                What can I do for you?
+                 Hello! I'm Goat Bot.
+                 What can I do for you?
                 ____________________________________________________________
                 """;
-        System.out.println(welcomeBanner);
-        Scanner scanner = new Scanner(System.in); //scanner object attached to stdin like keyboard
-        String userInput = scanner.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(
-                    "\n____________________________________________________________\n" +
-                            userInput + "\n" +
-                    "____________________________________________________________");
-            userInput = scanner.nextLine();
-        }
+
         // string if user says bye
         String farewell = """
                 ____________________________________________________________
-                Bye. Hope to see you again soon! Happy hooping :)
+                 Bye. Hope to see you again soon! Happy hooping :)
                 ____________________________________________________________
                 """;
+
+        System.out.println(welcomeBanner);
+        String divider = "    ____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        int listCounter = 0;
+        String[] listOfWords = new String[100];
+
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.println(divider);
+                for (int i = 1; i <= listCounter; i++) {
+                    System.out.println("     " + i + ". " + listOfWords[i - 1]);
+                }
+                System.out.println(divider);
+            } else {
+                listOfWords[listCounter++] = userInput;
+                System.out.println(divider);
+                System.out.println("     added: " + userInput);
+                System.out.println(divider);
+            }
+            userInput = scanner.nextLine();
+        }
         System.out.println(farewell);
     }
 }
